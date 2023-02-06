eb3d056e7f8377b2865d7df826e8bfc541079660
AygulSultan <jakypbaevaaygul@gmail.com>
2023-02-06 13:37:31 +0300

    DopZadanie

--- src/Esep.java
+++ src/Esep.java
@@ -14,9 +14,6 @@ public class Esep { public void calc(int numeric) {
             else {System.out.println("Bolunboyt");
                 break;
             }
-
-
-
         }
         else {
             break;
